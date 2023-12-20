7eaedef6b10a2c48e9c79651f892b75aeca31b4a
SnowballAntrobus <drgilmarin@icloud.com>
2023-12-19 21:57:55 -0800

    added ability to take box as input for mask predict

--- dreams_of_cloth/server/serializers.py
+++ dreams_of_cloth/server/serializers.py
@@ -8,6 +8,14 @@ class PointsSerializer(serializers.Serializer):
     pos_points = PointSerializer(many=True)
     neg_points = PointSerializer(many=True)
 
+class BoxSerializer(serializers.Serializer):
+    point1 = PointSerializer()
+    point2 = PointSerializer()
+
+class MaskPredictInputsSerializer(serializers.Serializer):
+    points = PointsSerializer(required=False)
+    box = BoxSerializer(required=False)
+
 class FilesForMaskSerializer(serializers.Serializer):
     json = serializers.FileField(allow_empty_file=False, required=True)
     image = serializers.ImageField()
